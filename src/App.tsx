0be8d8bada88238f3773592e84ac01793952c980
ak8abhinay <abhinay.kakunuri@gmail.com>
2025-10-23 13:56:35 +0000

    Created Cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import './App.css'
 import { Button } from './components/Button'
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
+import { Card } from './components/Card';
 
 function App() {
-  return <div>
-    <Button variant="primary" text="Add content" startIcon={<PlusIcon />}></Button>
-    <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}></Button>
+  return <div className='p-4'>
+    <div className='flex justify-end gap-4'>
+      <Button variant="primary" text="Add content" startIcon={<PlusIcon />}></Button>
+      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}></Button>
+    </div>
+    <div className='flex gap-4 pt-10'>
+       <Card link='https://youtu.be/gViEtIJ1DCw?si=KRd9jUHwYFowOxz_' title='Harkirat-video'/>
+    <Card link='https://youtu.be/gViEtIJ1DCw?si=KRd9jUHwYFowOxz_' title='100xdevs'/>
+    </div>
+   
   </div>
 }
 
 export default App
+
